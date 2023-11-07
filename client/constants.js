939d4b0bf64efe307a54c1efb0e55346b28cc458
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2023-11-07 13:31:27 -0300

    atualizacao constantes e aviso

--- client/constants.js
+++ client/constants.js
@@ -1,3 +1,5 @@
+// IMPORTANTE: SEMPRE QUE ATUALIZAR ALGO AQUI, TAMBÉM ATUALIZE O MESMO NO SERVER
+// o arquivo de constantes do servidor está disponível em: server/constants.js
 
-export const GAME_WIDTH = 1000
+export const GAME_WIDTH = 640
 export const GAME_HEIGHT = 800
