7f011b13b95ff9dc8c1317315d6c583eea908da0
Carlos de França <carloshfm@hotmail.com>
2023-11-07 10:12:47 -0300

    nave rotacionando no sentido do movimento, troca de sprite por nave menor

--- client/Enemies/EnemyDesavisados.js
+++ client/Enemies/EnemyDesavisados.js
@@ -1,7 +1,11 @@
 
 export function EnemyDesavisadosOnAdd(enemy, id) {
-    //this.enemiesEntities[id] = this.add.rectangle(enemy.x, enemy.y, 32, 32, 0x808080);
-    this.enemiesEntities[id] = this.physics.add.sprite(enemy.x, enemy.y, 'ship_0012');
+    this.enemiesEntities[id] = this.physics.add.sprite(enemy.x, enemy.y, 'ship_0022');
+    if (enemy.x <= 0){
+        this.enemiesEntities[id].angle = 90;
+    } else {
+        this.enemiesEntities[id].angle = -90;
+    }
 
     enemy.onChange(() => {
         this.enemiesEntities[id].x = enemy.x;
