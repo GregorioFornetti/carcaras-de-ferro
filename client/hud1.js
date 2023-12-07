68ce291c9c768868ef614044abbd2a372efa3a04
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2023-12-06 23:57:57 -0300

    score usando asset funcionando

--- client/hud1.js
+++ client/hud1.js
@@ -32,9 +32,10 @@ export default class HUD1 extends Phaser.Scene {
     create () {
 
         // Variáveis de configuração
-        this.shipScoreGap = 20
-        this.scoreNumbersGap = 1
-        this.scoreYExtra = 1
+        this.shipScoreGap = 25
+        this.scoreNumbersGap = 10
+        this.scoreScale = 1
+        this.scoreYExtra = 10
         this.scoresConfig = [
             {
                 sprite: 'ship_2',
@@ -81,8 +82,7 @@ export default class HUD1 extends Phaser.Scene {
                 this.currentPlayers[id].score, 
                 this.scoresConfig[currentPlayerNumber].x + shipScoreGap, 
                 this.scoresConfig[currentPlayerNumber].y + this.scoreYExtra, 
-                this.scoresConfig[currentPlayerNumber].flow, 
-                this.scoreNumbersGap
+                this.scoresConfig[currentPlayerNumber].flow
             )
   
            
@@ -94,46 +94,51 @@ export default class HUD1 extends Phaser.Scene {
             for (let i = 0; i < this.currentPlayers[id].scoreImages.length; i++) {
                 this.currentPlayers[id].scoreImages[i].destroy()
             }
+            let shipScoreGap = this.shipScoreGap
+            if (this.currentPlayers[id].flow === 'right') {
+                shipScoreGap = -shipScoreGap  // Por algum motivo o texto da direita fica mais para direita por padrão, então tem que compensar
+            }
+
+            console.log(this.currentPlayers[id].x + shipScoreGap)
             this.currentPlayers[id].scoreImages = this.displayScore(
                 this.currentPlayers[id].score, 
-                this.currentPlayers[id].image.x + this.shipScoreGap, 
+                this.currentPlayers[id].image.x + shipScoreGap, 
                 this.currentPlayers[id].image.y + this.scoreYExtra, 
-                this.currentPlayers[id].flow, 
-                this.scoreNumbersGap
+                this.currentPlayers[id].flow
             )
         })
     }
 
-    displayScore(score, x, y, flow, scoreNumbersGap) {
+    displayScore(score, x, y, flow) {
         if (flow === 'left') {
-            return this.displayScoreLeft(score, x, y, scoreNumbersGap)
+            return this.displayScoreLeft(score, x, y)
         } else if (flow === 'right') {
-            return this.displayScoreRight(score, x, y, scoreNumbersGap)
+            return this.displayScoreRight(score, x, y)
         }
     }
 
-    displayScoreLeft(score, x, y, scoreNumbersGap) {
+    displayScoreLeft(score, x, y) {
         let scoreString = score.toString()
         let scoreSprites = []
         let scoreSpritesX = x
     
         for (let i = 0; i < scoreString.length; i++) {
-            scoreSprites.push(this.add.image(scoreSpritesX, y, 'number_' + scoreString[i]).setScale(0.75))
-            scoreSpritesX += scoreNumbersGap
+            scoreSprites.push(this.add.image(scoreSpritesX, y, 'number_' + scoreString[i]).setScale(this.scoreScale))
+            scoreSpritesX += this.scoreNumbersGap
         }
     
         return scoreSprites
     }
     
 
-    displayScoreRight(score, x, y, scoreNumbersGap) {
+    displayScoreRight(score, x, y) {
         let scoreString = score.toString()
         let scoreSprites = []
         let scoreSpritesX = x
     
         for (let i = scoreString.length - 1; i >= 0; i--) {
-            scoreSprites.push(this.add.image(scoreSpritesX, y, 'number_' + scoreString[i]).setScale(0.75))
-            scoreSpritesX -= scoreNumbersGap
+            scoreSprites.push(this.add.image(scoreSpritesX, y, 'number_' + scoreString[i]).setScale(this.scoreScale))
+            scoreSpritesX -= this.scoreNumbersGap
         }
     
         return scoreSprites
